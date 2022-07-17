97f50d43e588103adf984b76689cec996f5d57dd
trungnd9 <96941235+trungnd9@users.noreply.github.com>
2022-07-17 10:25:20 +0700

    update navigate bar

--- src/page/navigate-mobile/NavigateMobile.tsx
+++ src/page/navigate-mobile/NavigateMobile.tsx
@@ -1,44 +1,82 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
+import { useContext } from "react";
+import { ThemeContext } from "../../components/ContextTheme/ContextTheme";
 
 export default function NavigateMobile() {
+  const { theme } = useContext(ThemeContext);
   return (
     <div className="navigate-mobile">
-      <div className="list-item">
+      <div className={`list-item ${theme === "dark" && "list-item-dark"}`}>
         <ul>
           <a href="#home">
             <li className="item">
-              <i className="fa-solid fa-house"></i>
-              <span>Home</span>
+              <i
+                className={`fa-solid fa-house ${
+                  theme === "dark" && "item-dark"
+                }`}
+              ></i>
+              <span className={`${theme === "dark" && "text-dark"}`}>Home</span>
             </li>
           </a>
           <a href="#about">
             <li className="item">
-              <i className="fa-solid fa-address-card"></i>
-              <span>About</span>
+              <i
+                className={`fa-solid fa-address-card ${
+                  theme === "dark" && "item-dark"
+                }`}
+              ></i>
+              <span className={`${theme === "dark" && "text-dark"}`}>
+                About
+              </span>
             </li>
           </a>
           <a href="#experience">
             <li className="item">
-              <i className="fa-solid fa-clock-rotate-left"></i>
-              <span>Expe...</span>
+              <i
+                className={`fa-solid fa-clock-rotate-left ${
+                  theme === "dark" && "item-dark"
+                }`}
+              ></i>
+              <span className={`${theme === "dark" && "text-dark"}`}>
+                Expe...
+              </span>
             </li>
           </a>
           <a href="#services">
             <li className="item">
-              <i className="fa-solid fa-gear"></i>
-              <span>Skill</span>
+              <i
+                className={`fa-solid fa-gear ${
+                  theme === "dark" && "item-dark"
+                }`}
+              ></i>
+              <span className={`${theme === "dark" && "text-dark"}`}>
+                Skill
+              </span>
             </li>
           </a>
           <a href="#project">
             <li className="item">
-              <i className="fa-solid fa-file-lines"></i>
-              <span>Project</span>
+              <i
+                className={`fa-solid fa-file-lines ${
+                  theme === "dark" && "item-dark"
+                }`}
+              ></i>
+              <span className={`${theme === "dark" && "text-dark"}`}>
+                Project
+              </span>
             </li>
           </a>
           <a href="#contact">
             <li className="item">
-              <i className="fa-solid fa-user-check"></i>
-              <span>Contact</span>
+              <i
+                className={`fa-solid fa-user-check ${
+                  theme === "dark" && "item-dark"
+                }`}
+              >
+              </i>
+              <span className={`${theme === "dark" && "text-dark"}`}>
+                Contact
+              </span>
             </li>
           </a>
         </ul>
